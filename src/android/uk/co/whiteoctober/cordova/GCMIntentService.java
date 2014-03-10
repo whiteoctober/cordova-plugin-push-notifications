79fcb17bec548a6d7054d6d732fd3335e12b7491
Rich Sage <rich.sage@gmail.com>
2014-03-10 16:13:00 +0000

    WIP Android.

---
+++ src/android/uk/co/whiteoctober/cordova/GCMIntentService.java
@@ -0,0 +1,107 @@
+package uk.co.whiteoctober.cordova;
+
+import java.util.Set;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+
+public class GCMIntentService extends GCMBaseIntentService {
+
+    public static final String ME = "GCMReceiver";
+
+    @Override
+    public void onRegistered(Context context, String registrationId) {
+
+        Log.v(ME + ":onRegistered", "Registration ID arrived!");
+        Log.v(ME + ":onRegistered", registrationId);
+
+        JSONObject json;
+
+        try {
+            json = new JSONObject().put("event", "registered");
+            json.put("regid", registrationId);
+
+            Log.v(ME + ":onRegistered", json.toString());
+            try {
+                GCMPlugin.sendJavascript(json);
+            } catch (NullPointerException e) {
+                Log.e(ME + ":onRegistered", "NullPointerException, maybe viewport is not active?");
+            }
+
+        } catch (JSONException e) {
+            // No message to the user is sent, JSON failed
+            Log.e(ME + ":onRegistered", "JSON exception");
+        }
+    }
+
+    @Override
+    public void onUnregistered(Context context, String registrationId) {
+        Log.v(ME + ":onUnregistered: ", registrationId);
+    }
+
+    @Override
+    public void onMessage(Context context, Intent intent) {
+        Log.v(ME + ":onMessage", "Received GCM message");
+
+        // Extract the payload from the message
+        Bundle extras = intent.getExtras();
+        Log.v(ME + ":onMessage", extras.toString());
+        if (extras != null) {
+            try {
+                JSONObject json = new JSONObject();
+
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    Object o = extras.get(key);
+                    json.put(key, o.toString());
+                }
+
+                Log.v(ME + ":onMessage ", json.toString());
+
+                try {
+                    GCMPlugin.sendJavascript(json);
+                } catch (NullPointerException e) {
+                    Log.v(ME + ":onMessage", "Null exception, never mind.");
+                }
+            } catch (JSONException e) {
+                Log.e(ME + ":onMessage", "JSON exception");
+            }
+        }
+    }
+
+    @Override
+    public void onError(Context context, String errorId) {
+        try {
+            JSONObject json;
+            json = new JSONObject().put("event", "error");
+            json.put("msg", errorId);
+
+            Log.e(ME + ":onError ", json.toString());
+
+            GCMPlugin.sendJavascript(json);
+        } catch (JSONException e) {
+            Log.e(ME + ":onMessage", "JSON exception");
+        }
+    }
+
+    @Override
+    protected boolean onRecoverableError(Context ctxt, String errorMsg) {
+        Log.d(getClass().getSimpleName(), "onRecoverableError: " + errorMsg);
+
+        return (true);
+    }
+
+    @Override
+    protected String[] getSenderIds(Context context) {
+        String[] x = new String[1];
+        x[0] = context.getResources().getString(R.string.gcmSenderID);
+        return x;
+    }
+}
---
+++ src/android/uk/co/whiteoctober/cordova/GCMPlugin.java
@@ -0,0 +1,81 @@
+package uk.co.whiteoctober.cordova;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.google.android.gcm.GCMRegistrar;
+
+public class GCMPlugin extends CordovaPlugin {
+
+    public static final String ME = "GCMPlugin";
+
+    public static final String REGISTER = "register";
+    public static final String UNREGISTER = "unregister";
+
+    public static Plugin gwebView;
+    private static String gECB;
+    private static String gSenderID;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        PluginResult result = null;
+
+        Log.v(ME + ":execute", "action=" + action);
+
+        if (REGISTER.equals(action)) {
+
+            try {
+                JSONObject jo = new JSONObject(args.toString().substring(1, args.toString().length() - 1));
+
+                Log.v(ME + ":execute", jo.toString());
+                gECB = (String) jo.get("ecb");
+                gSenderID = (String) jo.get("senderID");
+
+                final String regId = GCMRegistrar.getRegistrationId(this.ctx.getContext());
+                if (regId.equals("")) {
+                    GCMRegistrar.register(this.cordova.getActivity(), gSenderID);
+                } else {
+                    Log.v(ME + ":execute", "GCM - already registered");
+                    JSONObject json = new JSONObject().put("event", "registered");
+                    json.put("regid", regId);
+                    this.sendJavascript(json);
+                }
+
+                Log.v(ME + ":execute", "GCM register called");
+
+                callbackContext.success();
+                return true;
+
+            } catch (JSONException e) {
+                Log.e(ME, "Got JSON Exception " + e.getMessage());
+                return false;
+            }
+        }
+
+        if (UNREGISTER.equals(action)) {
+            GCMRegistrar.unregister(this.cordova.getActivity());
+            Log.v(ME + ":" + UNREGISTER, "GCM unregister called");
+
+            return true;
+        }
+
+        // No idea what to do here
+        Log.e(ME, "Invalid action : " + action);
+
+        return false;
+    }
+
+
+    public static void sendJavascript(JSONObject _json) {
+        String _d = "javascript:" + gECB + "(" + _json.toString() + ")";
+        Log.v(ME + ":sendJavascript", _d);
+
+        super.webView.sendJavascript(_d);
+    }
+}
