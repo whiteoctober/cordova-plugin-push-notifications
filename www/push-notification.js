6732f91c661448199641e22fcdc2160918344f29
Rich Sage <rich.sage@gmail.com>
2014-03-11 14:32:55 +0000

    force string.

--- www/push-notification.js
+++ www/push-notification.js
@@ -3,7 +3,7 @@ var PushNotification = function() {};
 PushNotification.prototype = {
     register: function(senderID) {
         console.log("Calling cordova.exec");
-        cordova.exec(null, null, 'PushNotification', 'register', [{ sender_id: senderID }]);
+        cordova.exec(null, null, 'PushNotification', 'register', [{ sender_id: "" + senderID }]);
     }
 }
 
