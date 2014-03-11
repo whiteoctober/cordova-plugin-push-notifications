20e6aecbc1432d7240f265cf02ced5b5eda1ecd0
Rich Sage <rich.sage@gmail.com>
2014-03-11 10:56:20 +0000

    Module config.

--- www/push-notification.js
+++ www/push-notification.js
@@ -5,3 +5,7 @@ PushNotification.prototype = {
         cordova.exec(null, null, 'PushNotification', 'register', { ecb: "foo", senderID: "12345" });
     }
 }
+
+var plugin = new PushNotification();
+
+module.exports = plugin;
